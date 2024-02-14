bfc1c0949ca001fd2525ae48ad4cbf9c826e5318
Gabriel de Oliveira Fernandes <160058698+GabrielOFernades@users.noreply.github.com>
2024-02-14 15:53:54 -0300

    Update excluir_msg_disc.py

--- excluir_msg_disc.py
+++ excluir_msg_disc.py
@@ -1,14 +1,12 @@
 import pyautogui
 from time import sleep
 
-# position
-i = 0
 x = int(input('Digite o numero de mensagens para apagar: '))
-while i <= x :
+for i in range(0, x):
     pyautogui.click(x=1335, y=847)
-    sleep(0.6)
+    sleep(0.8)
     pyautogui.click(x=1335, y=847)
     pyautogui.click(x=1100, y=965)
     sleep(0.6)
     pyautogui.click(x=1171, y=703)
-    i = i+1
+    sleep(0.5)
